fda302051c655dae4e8cc28fbd5141c7d22f3da1
LeoLuo015 <leoluo0115@gmail.com>
2023-11-07 21:55:52 -0600

    Sleeping Barber Problem and adjacency list

--- p3/csmc.c
+++ p3/csmc.c
@@ -1,352 +1,220 @@
-#include <pthread.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
+#include <string.h> 
+#include <time.h>
 
-// bounded buffer size
-#define NUM_THREADS 10 // 生产者线程数 + 消费者线程数
+#define MAX_CUSTOMERS 5
+#define NUM_CHAIRS 3
 
-// sleeping barber problem for customer and coordinator(barber)
-sem_t mutex;
+// Define the semaphores for the barber, the customers, and the mutex
 sem_t barber_semaphore;
 sem_t customer_semaphore;
-sem_t Tutor_semaphore;
+sem_t mutex;
 
-typedef struct
+// Define a list to keep track of the waiting customers
+int *waiting_customers = NULL;
+int num_waiting = 0;
+int next_customer = 0;
+
+//Define a Priority Queue to keep track priority (MLFQ with adjacent list)
+int *head = NULL;
+int *e = NULL;
+int *ne = NULL;
+int idx = 0;
+
+// Define the init function for priority queue
+void
+initialize ()
 {
-  int id;
-  int priority;
-  unsigned int time;
-  int tutor_id;
-  int taken_help_num;
-  bool have_seated;
-} Student_Data;
+  head = (int *) malloc (NUM_CHAIRS * sizeof (int));
+  e = (int *) malloc (NUM_CHAIRS * sizeof (int));
+  ne = (int *) malloc (NUM_CHAIRS * sizeof (int));
 
-typedef struct
+  if (head == NULL || e == NULL || ne == NULL)
+    {
+      fprintf (stderr, "Memory allocation failed\n");
+      exit (1);
+    }
+  memset (head, -1, NUM_CHAIRS * sizeof (int));
+}
+
+// Define the add function, a -> b, add at end of linklist
+void
+add (int a, int b)
 {
-  Student_Data *data;
-  int capacity;
-  int size;
-  pthread_mutex_t mutex;    // 互斥锁，用于线程安全
-  pthread_cond_t not_full;  // 条件变量，用于通知生产者
-  pthread_cond_t not_empty; // 条件变量，用于通知消费者
-} MaxHeap;
-
-bool done = false;
-Student_Data *StudentInfo = NULL;
-MaxHeap *heap = NULL;
-int taken_seats = 0;
-int NUM_STUDENTS;
-int NUM_TUTORS;
-int HELPS;
-int NUM_CHAIRS;
-int TOTAL_THREAD;
-
-int compareStudent_Datas(const Student_Data *a, const Student_Data *b)
-{
-  if (a->priority > b->priority)
-  {
-    return 1;
-  }
-  else if (a->priority < b->priority)
-  {
-    return -1;
-  }
+  e[idx] = b;
+  ne[idx] = -1;
+  if (head[a] == -1)
+    {
+      head[a] = idx;
+    }
   else
-  {
-    if (a->time < b->time)
     {
-      return 1;
+      int cur = head[a];
+      while (ne[cur] != -1)
+  {
+    cur = ne[cur];
+  }
+      ne[cur] = idx;
     }
-    else if (a->time > b->time)
+  idx++;
+}
+
+// Define the delete at head functiion
+void
+Delete (int a)
+{
+  if (head[a] != -1)
     {
-      return -1;
+      head[a] = ne[head[a]];
     }
-    else
+}
+
+// Define the print function to print all node along with vertex i
+void
+printPath (int i)
+{
+  if (i >= NUM_CHAIRS)
     {
-      return 0;
+      fprintf (stderr, "Access Memory out of range of adjacent list\n");
+      exit (1);
     }
-  }
-}
+  printf ("%d -> ", i);
 
-MaxHeap *createMaxHeap(int capacity)
-{
-  MaxHeap *heap = (MaxHeap *)malloc(sizeof(MaxHeap)); // 分配内存
-  if (heap == NULL)
-  {
-    printf("Memory allocation failed.\n");
-    exit(1);
-  }
-  heap->data = (Student_Data *)malloc(sizeof(Student_Data) * capacity);
-  if (heap->data == NULL)
-  {
-    printf("Memory allocation failed.\n");
-    exit(1);
-  }
-  heap->capacity = capacity;
-  heap->size = 0;
-  pthread_mutex_init(&heap->mutex, NULL);    // 初始化互斥锁
-  pthread_cond_init(&heap->not_full, NULL);  // 初始化条件变量（通知生产者）
-  pthread_cond_init(&heap->not_empty, NULL); // 初始化条件变量（通知消费者）
-  return heap;
-}
-
-void swap(Student_Data *a, Student_Data *b)
-{
-  Student_Data temp = *a;
-  *a = *b;
-  *b = temp;
-}
-
-void maxHeapify(MaxHeap *heap, int index)
-{
-  int left = 2 * index + 1;
-  int right = 2 * index + 2;
-  int largest = index;
-
-  if (left < heap->size &&
-      compareStudent_Datas(&heap->data[left], &heap->data[largest]) > 0)
-  {
-    largest = left;
-  }
-
-  if (right < heap->size &&
-      compareStudent_Datas(&heap->data[right], &heap->data[largest]) > 0)
-  {
-    largest = right;
-  }
-
-  if (largest != index)
-  {
-    swap(&heap->data[index], &heap->data[largest]);
-    maxHeapify(heap, largest);
-  }
-}
-
-void insert(MaxHeap *heap, Student_Data value)
-{
-  pthread_mutex_lock(&heap->mutex); // 加锁
-  while (heap->size == heap->capacity)
-  {
-    // 堆已满，等待消费者的通知
-    pthread_cond_wait(&heap->not_full, &heap->mutex);
-  }
-
-  heap->data[heap->size] = value;
-  int current = heap->size;
-  heap->size++;
-
-  while (current > 0 && compareStudent_Datas(&heap->data[current],
-                                             &heap->data[(current - 1) / 2]) > 0)
-  {
-    swap(&heap->data[current], &heap->data[(current - 1) / 2]);
-    current = (current - 1) / 2;
-  }
-
-  pthread_cond_signal(&heap->not_empty); // 通知等待的消费者
-  pthread_mutex_unlock(&heap->mutex);    // 解锁
-}
-
-Student_Data extractMax(MaxHeap *heap)
-{
-  pthread_mutex_lock(&heap->mutex); // 加锁
-
-  while (heap->size == 0 && !done)
-  {
-    // 堆为空时，等待生产者的通知
-    pthread_cond_wait(&heap->not_empty, &heap->mutex);
-  }
-
-  if (heap->size == 0 && done)
-  {
-    pthread_mutex_unlock(&heap->mutex);
-    Student_Data emptyStudent_Data = {0, 0};
-    return emptyStudent_Data;
-  }
-
-  Student_Data max = heap->data[0];
-  heap->data[0] = heap->data[heap->size - 1];
-  heap->size--;
-  pthread_cond_signal(&heap->not_full); // 通知等待的生产者
-  maxHeapify(heap, 0);
-
-  pthread_mutex_unlock(&heap->mutex); // 解锁
-
-  return max;
-}
-
-void destroyMaxHeap(MaxHeap *heap)
-{
-  free(heap->data);
-  free(heap);
-  pthread_mutex_destroy(&heap->mutex);    // 销毁互斥锁
-  pthread_cond_destroy(&heap->not_full);  // 销毁条件变量
-  pthread_cond_destroy(&heap->not_empty); // 销毁条件变量
-}
-
-void *producerFunction(void *student_id)
-{
-  int id = *(int *)student_id;
-  StudentInfo[id].id = id;
-  StudentInfo[id].tutor_id = -1;
-  StudentInfo[id].taken_help_num = 0;
-  StudentInfo[id].have_seated = false;
-  sleep(rand() % 5 + 1);
-  while (1)
-  {
-    // sleep(1); // go back to programming for 1 second;
-
-    // try to ask for help from tutor, check if there are availabe seats in the queue
-    sem_wait(&mutex);
-    if (taken_seats < NUM_CHAIRS)
+  int cur = head[i];
+  while (cur != -1)
     {
-      taken_seats++;
-      int priority = -1 * StudentInfo[id].taken_help_num;
-      StudentInfo[id].priority = priority;
-      unsigned int time = clock();
-      StudentInfo[id].time = time;
-      StudentInfo[id].have_seated = true;
-      // St: Student x takes a seat. Empty chairs = <# of empty chairs>.
-      printf("S: Student %d takes a seat. Empty chairs = %d\n", id, NUM_CHAIRS - taken_seats);
-      sem_post(&mutex);
-      sem_post(&barber_semaphore);
-      sem_wait(&customer_semaphore);
-    }
-    else
-    {
-      // S: Student x found no empty chair. Will try again later
-      printf("S: Student %d found no empty chair. Will try again later.\n", id);
-      sem_post(&mutex);
-      sleep(rand() % 5 + 1);
-      continue;
+      printf ("%d -> ", e[cur]);
+      cur = ne[cur];
     }
 
-    // // wait to be tutored.
-    // while (StudentInfo[id].tutor_id == -1)
-    // {
-    // };
-
-    // // S: Student x received help from Tutor y.
-    // printf("S: Student %d received help from Tutor %d.\n", id, StudentInfo[id].tutor_id);
-
-    // // reset tutor id
-    // StudentInfo[id].tutor_id = -1;
-
-    // reset have been seated
-    StudentInfo[id].have_seated = false;
-  }
+  printf ("NULL\n");
 }
 
-void *coordinatorFunction(void *args)
+// Define the free function for priority queue
+void
+cleanup ()
+{
+  free (head);
+  free (e);
+  free (ne);
+}
+
+// Define the barber thread function
+void *
+barber (void *arg)
 {
   while (1)
+    {
+      printf ("The barber is sleeping...\n");
+      sem_wait (&barber_semaphore);
+      sem_wait (&mutex);
+      if (num_waiting > 0)
   {
-    // wait student's notification
-    sem_wait(&barber_semaphore);
-    sem_wait(&mutex);
-    if (taken_seats > 0)
-    {
-      // put student in the queue, it could acutally be done by student itself, but due to requirements we need coordinator to do it
-      int i;
-      for (i = 0; i < NUM_STUDENTS; i++)
-      {
-        if (StudentInfo[i].have_seated)
-        {
-          printf("in %d\n", StudentInfo[i].id);
-          insert(heap, StudentInfo[i]);
-        }
-      }
-      Student_Data top_element = extractMax(heap);
-      printf("Consumer: Student %d with highest priority: (priority: %d, time: %u)\n",
-             top_element.id, top_element.priority, top_element.time);
-      taken_seats--;
-      printf("The barber is cutting hair for customer %d\n", top_element.id);
-      sem_post(&mutex);
-      sleep(rand() % 5 + 1);
-      printf("The barber has finished cutting hair for customer %d\n", top_element.id);
-      sem_post(&customer_semaphore);
-    }
-    else
-    {
-      sem_post(&mutex);
-    }
+    int customer = waiting_customers[next_customer];
+    next_customer = (next_customer + 1) % NUM_CHAIRS;
+    num_waiting--;
+    printf ("The barber is cutting hair for customer %d\n", customer);
+    // this is where barber will put customer id inside the priority queue
+    sem_post (&mutex);
+    sleep (rand () % 5 + 1);
+    printf ("The barber has finished cutting hair for customer %d\n",
+      customer);
+    sem_post (&customer_semaphore);
   }
+      else
+  {
+    sem_post (&mutex);
+  }
+    }
+  return NULL;
 }
 
-void *consumerFunction(void *args)
+// Define the customer thread function
+void *
+customer (void *arg)
 {
-  // while (1)
-  // {
-  //   sleep(1);
-  //   Student_Data top_element = extractMax(heap);
-  //   printf("Consumer: Student_Data with highest priority: (priority: %d, time: %u)\n",
-  //          top_element.priority, top_element.time);
+  int customer_id = *(int *) arg;
+  // while (1) {
+  sleep (rand () % 5 + 1);
+  sem_wait (&mutex);
+  if (num_waiting < NUM_CHAIRS)
+    {
+      waiting_customers[(next_customer + num_waiting) % NUM_CHAIRS] =
+  customer_id;
+      num_waiting++;
+      printf ("Customer %d is waiting in the waiting room\n", customer_id);
+      sem_post (&mutex);
+      sem_post (&barber_semaphore);
+      sem_wait (&customer_semaphore);
+      printf ("Customer %d has finished getting a haircut\n", customer_id);
+    }
+  else
+    {
+      printf ("Customer %d is leaving because the waiting room is full\n",
+        customer_id);
+      sem_post (&mutex);
+    }
   // }
+
+  return NULL;
 }
 
-int main()
+int
+main ()
 {
-  // read in data
-  NUM_STUDENTS = 5;
-  NUM_TUTORS = 3;
-  NUM_CHAIRS = 3;
-  HELPS = 5;
-  TOTAL_THREAD = NUM_STUDENTS + NUM_TUTORS;
+  srand (time (NULL));
 
-  int thread_args[NUM_THREADS];
+  // Initialize waiting_customers
+  waiting_customers = (int *) malloc (NUM_CHAIRS * sizeof (int));
 
-  // init prority queue
-  heap = createMaxHeap(NUM_CHAIRS);
-  // init StudentInfo which is stored in heap
-  StudentInfo = (Student_Data *)malloc(sizeof(Student_Data) * TOTAL_THREAD);
+  // Initialize Priority Queue
+  initialize ();
 
-  // init threads for students, tutors, coordinator
-  pthread_t threads[NUM_TUTORS + NUM_STUDENTS];
-  pthread_t coordinator;
+//   // test
+//   add (0, 1);
+//   add (0, 3);
+//   add (0, 4);
+//   add (2, 8);
+//   printPath (0);
+//   Delete (0);
+//   printPath (0);
+//   printPath (1);
+//   printPath (2);
 
-  // create mutex and semaphore
 
-  sem_init(&mutex, 0, 1);
-  sem_init(&barber_semaphore, 0, 0);
-  sem_init(&customer_semaphore, 0, 0);
-  sem_init(&Tutor_semaphore, 0, 0);
+  // Initialize the semaphores
+  sem_init (&barber_semaphore, 0, 0);
+  sem_init (&customer_semaphore, 0, 0);
+  sem_init (&mutex, 0, 1);
 
-  if (pthread_create(&coordinator, NULL, &coordinatorFunction, (void *)0) != 0)
-  {
-    perror("Failed to create thread");
-  }
+  // Create a thread for the barber
+  pthread_t barber_thread;
+  pthread_create (&barber_thread, NULL, barber, NULL);
 
-  // create all threads
-  for (int i = 0; i < NUM_THREADS; i++)
-  {
-    thread_args[i] = i;
-    if (i < NUM_STUDENTS)
+  // Create a thread for each customer
+  pthread_t customer_threads[MAX_CUSTOMERS];
+  int customer_ids[MAX_CUSTOMERS];
+  for (int i = 0; i < MAX_CUSTOMERS; i++)
     {
-      if (pthread_create(&threads[i], NULL, &producerFunction, (void *)&thread_args[i]) != 0)
-      {
-        perror("Failed to create thread");
-      }
+      customer_ids[i] = i;
+      pthread_create (&customer_threads[i], NULL, customer, &customer_ids[i]);
     }
-    else
+
+  // Join the barber and customer threads
+  pthread_join (barber_thread, NULL);
+  for (int i = 0; i < MAX_CUSTOMERS; i++)
     {
-      // if (pthread_create(&threads[i], NULL, &consumerFunction, (void *)&thread_args[i]) != 0)
-      // {
-      //   perror("Failed to create thread");
-      // }
+      pthread_join (customer_threads[i], NULL);
     }
-  }
 
-  // wait till all thread finish
-  for (int i = 0; i < NUM_STUDENTS; i++)
-  {
-    pthread_join(threads[i], NULL);
-  }
-  pthread_join(coordinator, NULL);
-
-  // delete dynamic memory and semaphore
-  destroyMaxHeap(heap);
+  // Destroy the semaphores
+  sem_destroy (&barber_semaphore);
+  sem_destroy (&customer_semaphore);
+  sem_destroy (&mutex);
 
   return 0;
 }
